cf31c2ea0e9b5c19f1cfb4a14732c6a63aa11a7c
simonberinglarsen <sendsla@gmail.com>
2016-09-17 14:39:11 +0200

    server rename

--- src/docker/cronserver/data/ping.sh
+++ src/docker/cronserver/data/ping.sh
@@ -1,4 +1,4 @@
-/usr/bin/wget http://webserver:5004/autotransfer/run --spider
+/usr/bin/wget http://lompengweb:5004/autotransfer/run --spider
 #/bin/echo "$(date): called website" >> /data/log.txt
 
 
